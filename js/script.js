633053f761b98fb067a3d2b5f99a5a32c2a29da4
mdshamim114003 <mdshamim114003@gmail.com>
2023-03-01 00:36:41 +0600

    createBill function call & unncecessary code removed from main script

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
+import { createBill } from "./bill.js";
 import { createDb, get , insert} from "./db.js";
 
 createDb()
-insert('name', 'shamim')
-get('name')
+createBill()
+document.addEventListener('DOMContentLoaded', function() {
+  const elems = document.querySelectorAll('.modal');
+  const instances = M.Modal.init(elems, {opacity : 0.7});
+});
